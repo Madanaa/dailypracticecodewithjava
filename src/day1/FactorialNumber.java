8a0e45b45d6cc53e1013cfb6b9d03d6e7debd726
Madanaa <madandev1994@gmail.com>
2026-04-04 03:38:31 +0530

    add day1 code

---
+++ src/day1/FactorialNumber.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class FactorialNumber {
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+   public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
