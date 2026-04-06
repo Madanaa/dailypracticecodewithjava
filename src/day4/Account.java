4caf2c6ba6f6480258cc88101338b2b9a74581e6
Madanaa <madandev1994@gmail.com>
2026-04-07 00:17:31 +0530

    add day4 code

--- src/day4/Account.java
+++ src/day4/Account.java
@@ -1,5 +1,39 @@
 package day4;
 
 public class Account {
-    private Double balance;
+    private double balance;
+
+    public Account(double initialBalance){
+        if(initialBalance > 0) {
+            this.balance = initialBalance;
+        }
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance = balance + amount;
+            System.out.println("Successfully Deposit: " +amount);
+        } else {
+            System.out.println("Invalid Deposit Amount!");
+        }
+    }
+   public void withdraw(double amount) {
+        if(amount > 0 && amount <= balance) {
+            balance = balance - amount;
+            System.out.println("Successfully Withdraw: "+amount);
+        }else {
+            System.out.println("Insufficient Balance or Invalid Amount!");
+        }
+   }
+
+   public double getBalance() {
+        return balance;
+   }
+
+    static void main() {
+  Account myAcc = new Account(1000);
+     myAcc.deposit(500);
+     myAcc.withdraw(200);
+        System.out.println(myAcc.getBalance());
+    }
 }
