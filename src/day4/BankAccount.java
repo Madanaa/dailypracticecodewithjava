97ff1ee1c0be3a9ef27313fb347b75da028ea092
Madanaa <madandev1994@gmail.com>
2026-04-06 23:26:08 +0530

    add day4 code

--- src/day4/BankAccount.java
+++ src/day4/BankAccount.java
@@ -1,4 +1,13 @@
 package day4;
 
 public class BankAccount {
+    private double balance;
+
+    private void deposit(double amount)
+    {
+        balance = balance + amount;
+    }
+    public  double getBalance() {
+        return balance;
+    }
 }
