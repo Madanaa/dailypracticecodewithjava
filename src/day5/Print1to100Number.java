0d54af020fbf012d60e8c12e9b36bc21103c7e7b
Madanaa <madandev1994@gmail.com>
2026-04-07 22:14:48 +0530

    add day5 code

---
+++ src/day5/Print1to100Number.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Print1to100Number {
+    public static void getNumber(int n) {
+        for (int i = 1; i <=n; i++) {
+            System.out.println(" "+i);
+        }
+    }
+
+    static void main() {
+        getNumber(100);
+    }
+}
