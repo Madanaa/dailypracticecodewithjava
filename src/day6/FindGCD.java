c223dc74d675ec5242c095ec264e529e65b14708
Madanaa <madandev1994@gmail.com>
2026-04-08 23:27:49 +0530

    add day6 code

---
+++ src/day6/FindGCD.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class FindGCD {
+    public static int findGCD(int a, int b) {
+        while(b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+    public static void main(String[]args) {
+        int a = 24;
+        int b = 36;
+        System.out.println(findGCD(a,b));
+    }
+}
