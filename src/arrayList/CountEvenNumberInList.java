92fbc9b4b95d6b0d64d1d86d2d367efb10f93a39
Madanaa <madandev1994@gmail.com>
2026-05-03 20:39:47 +0530

    add  code

--- src/arrayList/CountEvenNumberInList.java
+++ src/arrayList/CountEvenNumberInList.java
@@ -1,4 +1,28 @@
 package arrayList;
 
+import java.util.List;
+
 public class CountEvenNumberInList {
+    public static int countEven(List<Integer>list) {
+        int count = 0;
+        for (int num : list) {
+            if(num % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        java.util.List<Integer> list = new java.util.ArrayList<>();
+        list.add(10);
+        list.add(15);
+        list.add(20);
+        list.add(25);
+        list.add(30);
+
+        System.out.println("List: " + list);
+        System.out.println("Count of even numbers: " + countEven(list));
+    }
+
 }
--- src/arrayList/SumOfArrayList.java
+++ src/arrayList/SumOfArrayList.java
@@ -1,4 +1,27 @@
 package arrayList;
 
+import java.util.List;
+
 public class SumOfArrayList {
+  public static int calculateSum(List<Integer> list) {
+      int sum = 0;
+      for (int num : list) {
+          sum = sum + num;
+      }
+      return sum;
+  }
+
+
+    public static void main(String[] args) {
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println("List: " + list);
+        System.out.println("Sum of elements: " + calculateSum(list));
+    }
+
 }
