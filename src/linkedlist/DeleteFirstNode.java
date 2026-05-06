70acded3f82f6c70ad36aaff2d61d8714273c5d4
Madanaa <madandev1994@gmail.com>
2026-05-06 19:19:38 +0530

    add  code

---
+++ src/linkedlist/DeleteFirstNode.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+
+
+public class DeleteFirstNode {
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+}
