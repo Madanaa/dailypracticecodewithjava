77bfe7c78e809cdf66183c5ade9a48131f24952f
Madanaa <madandev1994@gmail.com>
2026-06-06 23:26:41 +0530

    add  code

--- src/day23/AngramString.java
+++ src/day23/AngramString.java
@@ -5,14 +5,14 @@ import java.util.*;
 public class AngramString {
     public List<List<String>> anagrams(String[] str){
         Map<String,List<String>> ans = new HashMap<>();
-        for (int i = 0; i < str.length; i++){
-            char[] arr = str[i].toCharArray();
+        for (String s : str) {
+            char[] arr = s.toCharArray();
             Arrays.sort(arr);
             String sorted = new String(arr);
-            if (!ans.containsKey(sorted)){
-                ans.put(sorted,new ArrayList<>());
+            if (!ans.containsKey(sorted)) {
+                ans.put(sorted, new ArrayList<>());
             }
-            ans.get(sorted).add(str[i]);
+            ans.get(sorted).add(s);
         }
       return new ArrayList<>(ans.values());
     }
